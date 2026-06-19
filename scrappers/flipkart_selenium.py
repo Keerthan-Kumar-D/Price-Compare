e9479e0f20e0dff869ae4666871efdaf8b40f686
Keerthan-Kumar-D <keerthankd1802@gmail.com>
2026-06-19 20:22:09 +0530

    binary location addition

--- scrappers/flipkart_selenium.py
+++ scrappers/flipkart_selenium.py
@@ -25,6 +25,7 @@ def search_flipkart_selenium(search_query, max_wait=5):
     options.add_argument('--disable-blink-features=AutomationControlled')
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
     options.add_experimental_option('useAutomationExtension', False)
+    options.binary_location = "/usr/bin/google-chrome"
     options.add_argument(
         'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
     )
